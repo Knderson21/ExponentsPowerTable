022b83c3f1b2f70b1c3ff666a0d4eeee59573bdb
Krista <knderson21@gmail.com>
2022-06-23 17:08:00 -0400

    First commit

---
+++ ExponentsPowerTable/Program.cs
@@ -0,0 +1,42 @@
+﻿// prompts the user to enter an integer.
+
+Console.WriteLine("Learn your squares and cubes!");
+
+bool tryAgain = true;
+while (tryAgain)
+{
+    
+        Console.WriteLine("Enter an integer: ");
+        int choice = int.Parse(Console.ReadLine());
+
+    
+
+        // display first rows
+        Console.WriteLine("Number\tSquared\tCubed");
+        Console.WriteLine("======\t=======\t=====");
+
+        // displays chart data
+        for (int i = 1; i <= choice; i++)
+        {
+            Console.WriteLine(i + "\t" + NumberSquared(i) + "\t" + NumberCubed(i));
+        }
+
+        static int NumberSquared(int choice)
+        {
+            return (choice * choice);
+        }
+        static int NumberCubed(int choice)
+        {
+            return (choice * choice * choice);
+        }
+
+        Console.WriteLine("Would you like to restart? y/n: ");
+        string restart = Console.ReadLine().ToLower();
+
+        if (restart != "y")
+        {
+            tryAgain = false;
+        }
+    } 
+
+Console.WriteLine("\nGoodbye!");
